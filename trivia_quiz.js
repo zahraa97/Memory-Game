96db817428c7006f59d5cbb527c1418f2d1f2c26
zahraa97 <43418232+zahraa97@users.noreply.github.com>
2018-09-30 22:32:20 -0500

    Add files via upload

---
+++ trivia_quiz.js
@@ -0,0 +1,50 @@
+function question1(){
+	var element = document.getElementById("question1Id");
+	element.innerHTML = "Louis Pasteur"
+}
+
+function question2(){
+	var element = document.getElementById("question2Id");
+	element.innerHTML = "8,000 miles";
+}
+function question3(){
+	var element = document.getElementById("question3Id");
+	element.innerHTML = "Czech Republic";
+}
+function question4(){
+	var element = document.getElementById("question4Id");
+	element.innerHTML = "Red, Yellow, Blue";
+}
+function question5(){
+	var element = document.getElementById("question5Id");
+	element.innerHTML = "April 23, 1564";
+}
+function question6(){
+	var element = document.getElementById("question6Id");
+	element.innerHTML = "1989";
+}
+function question7(){
+	var element = document.getElementById("question7Id");
+	element.innerHTML = "The Diamond Dutra, dated at 868 AD";
+}
+function question8(){
+	var element = document.getElementById("question8Id");
+	element.innerHTML = "Eyebrows";
+}
+function question9(){
+	var element = document.getElementById("question9Id");
+	element.innerHTML = "Pomology";
+}
+function question10(){
+	var element = document.getElementById("question10Id");
+	element.innerHTML = "Tumeric";
+}
+function question11(){
+	var element = document.getElementById("question11Id");
+	element.innerHTML = "Faster than light";
+}
+function question12(){
+	var element = document.getElementById("question12Id");
+	element.innerHTML = "A wisdom tooth";
+}
+
